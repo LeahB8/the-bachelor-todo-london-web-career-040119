a78aee93f8bc00f2a63bd89709dedb1b91aa2391
Leah Boyd <leahboyd08@gmail.com>
2019-04-03 07:33:37 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,40 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do | key, value |
+    season.each do | first_name, stat |
+      if value["status"] == "Winner"
+        return value["name"].first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do | key, value |
+    if value["occupation"] == occupation
+      return value["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do | key, value |
+    if value["hometown"] == hometown
+      count = hometown 
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do | key, value |
+    if value["hometown"] == hometown
+      return value["occupation"]
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average = data.sum {| x | x(season) } / array.size
+    return average
+  end
 end
